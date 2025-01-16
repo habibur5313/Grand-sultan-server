20474738f1c696906287c53a97a3fac5423d2c33
habibur5313 <habiburmamun313@gmail.com>
2025-01-16 15:05:56 +0600

    agreements related apis added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -25,9 +26,67 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 const document = client.db('BuildCareDB')
+// collections
 const ApartmentsCollection = document.collection('apartments')
+const userCollection = document.collection('users')
+const agreementCollection = document.collection('agreements')
+
+ // jwt related api
+ app.post('/jwt', async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10d' });
+  res.send({ token });
+})
+
+// middleware
+const verifyToken = (req, res, next) => {
+  // console.log('inside verify token', req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.email;
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  const isAdmin = user?.role === 'admin';
+  if (!isAdmin) {
+    return res.status(403).send({ message: 'forbidden access' });
+  }
+  next();
+}
+
+// users related apis
+app.post('/users', async (req, res) => {
+  const user = req.body;
+  const query = { email: user.email }
+  const existingUser = await userCollection.findOne(query);
+  if (existingUser) {
+    return res.send({ message: 'user already exists', insertedId: null })
+  }
+  const result = await userCollection.insertOne(user);
+  res.send(result);
+});
+
+//  apartments related apis
 app.get('/apartments',async(req,res) =>  {
   const result = await ApartmentsCollection.find().toArray()
+  res.send(result)
+})
+
+// agreements related apis
+app.post('/agreements',async(req,res) => {
+  const agreement = req.body;
+  const result = await agreementCollection.insertOne(result)
   res.send(result)
 })
     // Send a ping to confirm a successful connection
